90318ad809fed9089eeab763fffebffa90a69cfc
kyleb <kyleb@DESKTOP-LDJID95>
2020-01-31 21:01:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserInputKyleBigelow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string response;
+
+            Console.WriteLine("Hi, this is Kyle Bigelow! " +
+                 "Tell me how you are feeling today, then hit Enter.");
+
+            response = Console.ReadLine();
+
+            Console.WriteLine("You typed: "+ response);
+            
+        }
+    }
+}
